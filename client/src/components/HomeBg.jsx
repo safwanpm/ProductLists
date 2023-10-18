72f253ecb7e16dc14fa1564423f3e152b991ff9b
safwanpm <safwanpm0@gmail.com>
2023-10-18 16:34:47 +0530

    update

--- client/src/components/HomeBg.jsx
+++ client/src/components/HomeBg.jsx
@@ -1,53 +1,93 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
+// function HomeBg() {
+//   const [data, setdata] = useState({})
+//   const HandleCategory = () => {};
 
-function HomeBg() {
-  const HandleCategory = () => {};
+//   const [categories, setCategories] = useState([]);
+//   useEffect(() => {
+//     // axios.get("http://localhost:4000/save/view-category")
+//     axios
+//       .get("https://productlist-04vn.onrender.com/save/view-category")
+//       .then((res) => {
+//         setCategories(res.data.data);
+//         console.log(res);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching categories:", error);
+//       });
+//   }, []);
 
-  const [category, setCategory] = useState([]);
+//   const [subCategories, setSubCategories] = useState([]);
+//   useEffect(() => {
+//     if (data.category) {
+//       // axios.get(`http://localhost:4000/save/view-sub_category/${data.category}`)
+//       axios
+//         .get(
+//           `https://productlist-04vn.onrender.com/save/view-sub_category/${data.category}`
+//         )
+//         .then((res) => {
+//           setSubCategories(res.data.data);
+//           console.log(res);
+//         })
+//         .catch((error) => {
+//           console.error("Error fetching sub-categories:", error);
+//         });
+//     }
+//   }, [data.category]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/save/view-category")
-      .then((res) => {
-        console.log(res);
-        setCategory(res.data.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching categories:", error);
-      });
-  }, []);
+//   return (
+//     <div className="container mb-5 justify-center align-center mt-5">
+//       <div className="row" id="">
+//         <div className="col-lg-6">
+//           <div className="dropdown">
+//             <a
+//               className="btn btn-primary card"
+//               role="button"
+//               data-bs-toggle="dropdown"
+//               aria-expanded="false"
+//             >
+//               Product
+//             </a>
+//             <div>
+//             <select
+//                 onChange={HandleCategory}
+//                 name="category"
+//                 className="form-control"
+//                 id="category"
+//                 value={data.category}
+//               >
+//                 <option value="">select</option>
+//                 {categories.map((category, key) => (
+//                   <option key={key} value={category.category}>
+//                     {category.category}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+//             <div>
+//             <select
+//                 onChange={HandleCategory}
+//                 name="sub_category"
+//                 className="form-control"
+//                 id="sub_category"
+//                 value={data.sub_category}
+//               >
+//                 <option value="">select</option>
+//                 {subCategories.map((sub_category, key) => (
+//                   <option key={key} value={sub_category.sub_category}>
+//                     {sub_category.sub_category}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+           
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-  return (
-    <div className="container mb-5 justify-center align-center mt-5">
-      <div className="row" id="">
-        <div className="col-lg-6">
-          <div className="dropdown">
-            <a
-              className="btn btn-primary card"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Product
-            </a>
-            <ul className="dropdown-menu ">
-              <li>
-                {category.map((cat, key) => (
-                  <div className="card" style={{ width: "" }}>
-                    <div className="card-body" onClick={HandleCategory}>
-                      <h4 key={key}>{cat.category}</h4>
-                    </div>
-                  </div>
-                ))}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default HomeBg;
+// export default HomeBg;
